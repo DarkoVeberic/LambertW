dc55608ea09dbe7ebe7157418a4e680e169807d1
Darko Veberic <darko.veberic@ijs.si>
2014-04-17 15:11:59 +0200

    under construction

---
+++ LambertW.h
@@ -0,0 +1,29 @@
+#ifndef _utl_LambertW_h_
+#define _utl_LambertW_h_
+
+#include <limits>
+
+
+namespace utl {
+
+  /*template<int branch>
+  double LambertWApproximation(const double x);*/
+
+  template<int branch>
+  double LambertW(const double x);
+
+  inline
+  double
+  LambertW(const int branch, const double x)
+  {
+    switch (branch) {
+    case -1: return LambertW<-1>(x);
+    case  0: return LambertW<0>(x);
+    default: return std::numeric_limits<double>::quiet_NaN();
+    }
+  }
+
+}
+
+
+#endif
--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Observatory](http://www.auger.org).
 The work is described in the following publications:
 * [arXiv:1003.1628](http://arxiv.org/abs/1003.1628)
 * [arXiv:1209.0735](http://arxiv.org/abs/1209.0735) and [Comp. Phys. Comm. **183** (2012) 2622-2628](http://dx.doi.org/10.1016/j.cpc.2012.07.008)
+
+
+
+## Under construction
+
+Stay tuned...
